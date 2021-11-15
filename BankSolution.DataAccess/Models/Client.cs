3008d18781d225e56306c10a248e9439fc77727e
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-11-15 19:50:08 +0200

    HW4 completed.

---
+++ BankSolution.DataAccess/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankSolution.DataAccess.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public string Location { get; set; }
+        public List<Project> projects { get; set; }
+    }
+}
