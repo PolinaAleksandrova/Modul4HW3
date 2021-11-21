5311efea32d249bc2d8caa2da02e0027118711e4
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-11-22 00:02:04 +0200

    last commit

--- Modul4HW3/Program.cs
+++ Modul4HW3/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Linq;
+using BankSolution.DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Modul4HW3;
 
 namespace BankSolution.DataAccess
 {
@@ -14,13 +16,13 @@ namespace BankSolution.DataAccess
             using (var appContext = appContextFactory.CreateDbContext(args))
             {
                 var leftJoin = (from o in appContext.Offices
-                                join e in appContext.Employees on o.Id equals e.OfficeId into officesEmployes
+                                join e in appContext.Employees on o.OfficeId equals e.OfficeId into officesEmployes
                                 from oe in officesEmployes.DefaultIfEmpty()
-                                select new { Id = o.Id, OfficeLocation = o.Location, EmployeeName = oe.FirstName, TitleId = oe.TitleId })
+                                select new { Id = o.OfficeId, OfficeLocation = o.Location, EmployeeName = oe.FirstName, TitleId = oe.TitleId })
                                 .Join(
-                    bankContext.Titles,
+                    appContext.Titles,
                     oe => oe.TitleId,
-                    t => t.Id,
+                    t => t.TitleId,
                     (oe, t) => new { Id = oe.Id, OfficeLocation = oe.OfficeLocation, EmployeeName = oe.EmployeeName, TitleName = t.Name })
                                 .ToList();
 
@@ -29,15 +31,16 @@ namespace BankSolution.DataAccess
                 var employees = appContext.Employees.ToList();
                 employees[0].FirstName = "FirstName1";
                 employees[1].LastName = "LastName1";
-                bankContext.Employees.Update(employees[0]);
-                bankContext.Employees.Update(employees[1]);
+                appContext.Employees.Update(employees[0]);
+                appContext.Employees.Update(employees[1]);
                 var newEmployee = new Employee() { FirstName = "FirstName2", LastName = "LastName2", HiredDate = new DateTime(2019, 7, 5), OfficeId = 1, TitleId = 3 };
                 var newProject = new Project() { Name = "Name1", Budget = 6000000, StartedDate = DateTime.UtcNow, ClientId = 1 };
                 appContext.Employees.Add(newEmployee);
                 appContext.Projects.Add(newProject);
                 appContext.Employees.Remove(employees[1]);
                 var title = appContext.Employees.ToList().GroupBy(e => e.Title.Name).Select(x => x.Key).FirstOrDefault(y => !y.Contains('a'));
-                bankContext.SaveChanges();
+                appContext.SaveChanges();
             }
+        }
     }
 }
