093f3d43e99060f8e107eab69debfc20c850d01a
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2021-11-21 23:53:24 +0200

    hw 5 completed

--- Modul4HW3/Program.cs
+++ Modul4HW3/Program.cs
@@ -9,14 +9,35 @@ namespace BankSolution.DataAccess
     {
         public static void Main(string[] args)
         {
-            IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            var dbOptionsBuilder = new DbContextOptionsBuilder<AppContext>();
-            var connectionString = configuration.GetConnectionString("DataModel");
-            dbOptionsBuilder.UseSqlServer(connectionString, i => i.CommandTimeout(20));
-            dbOptionsBuilder.LogTo(System.Console.Write);
-            var appContext = new AppContext(dbOptionsBuilder.Options);
-            appContext.Database.Migrate();
-            appContext.SaveChanges();
-        }
+            var appContextFactory = new AppContextFactory();
+
+            using (var appContext = appContextFactory.CreateDbContext(args))
+            {
+                var leftJoin = (from o in appContext.Offices
+                                join e in appContext.Employees on o.Id equals e.OfficeId into officesEmployes
+                                from oe in officesEmployes.DefaultIfEmpty()
+                                select new { Id = o.Id, OfficeLocation = o.Location, EmployeeName = oe.FirstName, TitleId = oe.TitleId })
+                                .Join(
+                    bankContext.Titles,
+                    oe => oe.TitleId,
+                    t => t.Id,
+                    (oe, t) => new { Id = oe.Id, OfficeLocation = oe.OfficeLocation, EmployeeName = oe.EmployeeName, TitleName = t.Name })
+                                .ToList();
+
+                var dateDifference = appContext.Employees.Select(e => DateTime.UtcNow - e.HiredDate);
+
+                var employees = appContext.Employees.ToList();
+                employees[0].FirstName = "FirstName1";
+                employees[1].LastName = "LastName1";
+                bankContext.Employees.Update(employees[0]);
+                bankContext.Employees.Update(employees[1]);
+                var newEmployee = new Employee() { FirstName = "FirstName2", LastName = "LastName2", HiredDate = new DateTime(2019, 7, 5), OfficeId = 1, TitleId = 3 };
+                var newProject = new Project() { Name = "Name1", Budget = 6000000, StartedDate = DateTime.UtcNow, ClientId = 1 };
+                appContext.Employees.Add(newEmployee);
+                appContext.Projects.Add(newProject);
+                appContext.Employees.Remove(employees[1]);
+                var title = appContext.Employees.ToList().GroupBy(e => e.Title.Name).Select(x => x.Key).FirstOrDefault(y => !y.Contains('a'));
+                bankContext.SaveChanges();
+            }
     }
 }
